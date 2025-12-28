1ced2090205c791f97e6778ff052cddcb0e1103c
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-12-28 21:12:59 +0700

    put the plugin back

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
+import type { Plugin } from "vite";
 import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { datExport } from "./vite-plugin-poe-dat-export";
 import {
   CdnBundleLoader,
   FileLoader,
 } from "./node_modules/pathofexile-dat/dist/cli/bundle-loaders";
 import { exportTables } from "./node_modules/pathofexile-dat/dist/cli/export-tables";
+import * as path from "node:path";
+import fs from "node:fs/promises";
 
 interface IncursionRoom {
   _index: number;
@@ -35,26 +37,91 @@ interface IncursionRoomPerLevel {
   Description2: string;
 }
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    datExport({
-      CdnBundleLoader,
-      FileLoader,
-      exportTables,
-      tables: [
-        "Incursion2Rooms",
-        "Incursion2RoomPerLevel",
-        "Incursion2Medallions",
-        "Mods",
-        "Stats",
-      ],
-      async onProcessLang(lang, load) {
+const LANGS = [
+  "English",
+  "French",
+  "German",
+  "Japanese",
+  "Korean",
+  "Portuguese",
+  "Russian",
+  "Spanish",
+  "Thai",
+  "Traditional Chinese",
+];
+
+const includeAll = [] as string[];
+includeAll.includes = () => true;
+
+export interface DatExportOptions {
+  tables: (string | { name: string; columns: string[] })[];
+  onProcessLang?: (
+    lang: string,
+    load: <T>(file: string) => Promise<T[]>,
+  ) => Promise<Record<string, unknown>>;
+  outputRoot?: string;
+}
+
+const datExport = (options: DatExportOptions): Plugin => {
+  const { tables, outputRoot = path.join("src", "data", "generated") } =
+    options;
+  let cacheDir = ".cache";
+
+  async function load(lang: string, file: string) {
+    const tablePath = path.join("tables", lang, `${file}.json`);
+    const content = await fs.readFile(tablePath, "utf-8");
+    return JSON.parse(content);
+  }
+
+  return {
+    name: "dat-export",
+    configResolved(config) {
+      cacheDir = path.join(config.cacheDir, "poe-data");
+    },
+
+    async buildStart() {
+      const cdnUrl = await fetch("https://ggpk.exposed/version?poe=2").then(
+        (r) => r.text(),
+      );
+      const patch = cdnUrl.split("/").find((v) => v.match(/\d+(\.\d+)+/));
+      if (!patch) {
+        throw new Error("Could not find patch version from cdnUrl");
+      }
+      const cdnBundleLoader = await CdnBundleLoader.create(
+        path.join(cacheDir, "bundles"),
+        patch,
+      );
+
+      const loader = await FileLoader.create(cdnBundleLoader);
+      const exportedFiles = path.join(cacheDir, "exported");
+
+      console.log(`Exporting data for version`, patch, "to", exportedFiles);
+      for (const tr of LANGS) {
+        await fs.mkdir(path.join("tables", tr), { recursive: true });
+      }
+
+      await exportTables(
+        {
+          patch,
+          translations: LANGS,
+          tables: tables.map((table) =>
+            typeof table === "string"
+              ? { name: table, columns: includeAll }
+              : table,
+          ),
+        },
+        exportedFiles,
+        loader,
+      );
+
+      await fs.mkdir(outputRoot, { recursive: true });
+      for (const lang of LANGS) {
         const combined: Record<string, unknown> = {};
-        const rooms = (await load("Incursion2Rooms")) as IncursionRoom[];
-        const mods = (await load("Mods")) as Record<string, number>[];
-        const stats = (await load("Stats")) as Record<string, string>[];
+        const rooms = (await load(lang, "Incursion2Rooms")) as IncursionRoom[];
+        const mods = (await load(lang, "Mods")) as Record<string, number>[];
+        const stats = (await load(lang, "Stats")) as Record<string, string>[];
         const levels = (await load(
+          lang,
           "Incursion2RoomPerLevel",
         )) as IncursionRoomPerLevel[];
 
@@ -62,7 +129,10 @@ export default defineConfig({
           rooms.map((room) => [room._index, room.Id]),
         );
 
-        combined.Incursion2Medallions = await load("Incursion2Medallions");
+        combined.Incursion2Medallions = await load(
+          lang,
+          "Incursion2Medallions",
+        );
         combined.Incursion2Rooms = Object.fromEntries(
           rooms.map((room) => {
             const Levels: Record<string, unknown>[] = [];
@@ -92,8 +162,28 @@ export default defineConfig({
             ];
           }),
         );
-        return combined;
-      },
+        await fs.writeFile(
+          path.join(outputRoot, `${lang}.json`),
+          JSON.stringify(combined, null, 2),
+        );
+      }
+
+      await fs.rm("./tables", { recursive: true });
+    },
+  };
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    datExport({
+      tables: [
+        "Incursion2Rooms",
+        "Incursion2RoomPerLevel",
+        "Incursion2Medallions",
+        "Mods",
+        "Stats",
+      ],
     }),
     react(),
   ],
